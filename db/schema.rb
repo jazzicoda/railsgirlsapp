3906ec7109ef816375666e67f2336bf57f65fedf
Jasmine Ofaolain <jasmineofaolain@gmail.com>
2021-07-03 13:55:06 +1000

    added comment functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_004812) do
+ActiveRecord::Schema.define(version: 2021_07_03_020905) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
